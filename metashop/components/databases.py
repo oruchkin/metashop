f475a42fd589f0c7fdbf01239e1e1555034c5212
Oleg <oruchkin@bk.ru>
2022-10-13 12:35:36 +0300

    postgres template added

--- metashop/components/databases.py
+++ metashop/components/databases.py
@@ -8,3 +8,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+#настройки для postgresql
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'db_name',
+#         'USER': 'username',
+#         'PASSWORD': 'password!',
+#         'HOST': 'adress.yandex.ru', 
+#         'PORT': '3306',
+#         'ssl-mode': 'DISABLED',
+#     }
+# }
