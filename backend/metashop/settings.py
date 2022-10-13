477e73b1c17714aa72b678d7e3fd913fe0a3f368
Oleg <oruchkin@bk.ru>
2022-10-13 13:20:01 +0300

    trusted_origins

--- backend/metashop/settings.py
+++ backend/metashop/settings.py
@@ -18,7 +18,7 @@ include(
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["['http://127.0.0.1', 'https://127.0.0.1']"]
 
 
 #SECRET_KEY = os.environ.get('SECRET_KEY', default=None),
